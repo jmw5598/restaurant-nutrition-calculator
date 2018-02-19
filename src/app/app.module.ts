4b621a56f2ae7eccaaaeab1e76ba18c2993a5fe9
Jason White <jmw5598@gmail.com>
2018-02-19 16:15:52 -0500

    imported nutritional-calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
+import { NutritionalCalculatorModule } from './nutritional-calculator/nutritional-calculator.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
+    NutritionalCalculatorModule,
     SharedModule
   ],
   providers: [],
