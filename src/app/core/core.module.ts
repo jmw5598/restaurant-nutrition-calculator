67e1e86098c0474a39ea2291c1aef6a4f4e2354f
Jason White <jmw5598@gmail.com>
2018-02-19 18:29:57 -0500

    updated core modele with new services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 
+import { NutritionalCalculatorService } from './service/nutritional-calculator.service';
+import { NutritionalFactsUtilityService } from './service/nutritional-facts-utility.service';
+
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
-  providers: []
+  providers: [
+    NutritionalCalculatorService,
+    NutritionalFactsUtilityService
+  ]
 })
 export class CoreModule {}
