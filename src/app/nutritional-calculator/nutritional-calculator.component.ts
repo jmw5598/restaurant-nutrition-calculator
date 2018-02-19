69c4f0be50c5741262d44a3139b9d7d96f56ed4e
Jason White <jmw5598@gmail.com>
2018-02-19 16:15:23 -0500

    created nutritional-calculator module and base component

---
+++ src/app/nutritional-calculator/nutritional-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qdoba-nutritional-calculator',
+  templateUrl: './nutritional-calculator.component.html',
+  styleUrls: ['./nutritional-calculator.component.css']
+
+})
+export class NutritionalCalculatorComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
---
+++ src/app/nutritional-calculator/nutritional-calculator.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { NutritionalCalculatorComponent } from './nutritional-calculator.component';
+@NgModule({
+  declarations: [
+    NutritionalCalculatorComponent
+  ],
+  imports: [],
+  exports: [
+    NutritionalCalculatorComponent
+  ]
+})
+export class NutritionalCalculatorModule {}
