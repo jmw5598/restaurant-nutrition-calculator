c8409a1f67100021bfbc8496989bf674db940da5
Jason White <jmw5598@gmail.com>
2018-02-20 16:25:13 -0500

    created skeleton component for menu heading

---
+++ src/app/nutritional-calculator/menu/menu-heading/menu-heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qdoba-menu-heading',
+  templateUrl: './menu-heading.component.html',
+  styleUrls: ['../menu.component.css', './menu-heading.component.css']
+})
+export class MenuHeadingComponent {
+
+  constructor() {}
+
+}
