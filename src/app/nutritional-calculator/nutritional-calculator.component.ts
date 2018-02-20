c582f9ed7110b646f03c3811cb46c7f2fdfd3024
Jason White <jmw5598@gmail.com>
2018-02-20 16:20:24 -0500

    styled, and added menu, entree, and nutritional facts components to template

--- src/app/nutritional-calculator/nutritional-calculator.component.ts
+++ src/app/nutritional-calculator/nutritional-calculator.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NutritionalCalculatorService } from '../core/service/nutritional-calculator.service';
+
+import { Ingredient } from '../shared/model/ingredient.model';
+import { IngredientCategory } from '../shared/model/ingredient-category.enum';
+import { NutritionalFacts } from '../shared/model/nutritional-facts.model';
+import { FilterByIngredientCategoryPipe } from '../shared/pipe/filter-by-ingredient-category.pipe';
+
 @Component({
   selector: 'qdoba-nutritional-calculator',
   templateUrl: './nutritional-calculator.component.html',
@@ -8,8 +15,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NutritionalCalculatorComponent implements OnInit {
 
-  constructor() {}
+  ingredients: Array<Ingredient>;
+  ingredientCategory = IngredientCategory;
 
-  ngOnInit() {}
+  constructor(
+    private calcService: NutritionalCalculatorService
+  ) {}
+
+  ngOnInit() {
+    this.ingredients = this.calcService.getIngredients();
+  }
 
 }
--- src/app/nutritional-calculator/nutritional-calculator.module.ts
+++ src/app/nutritional-calculator/nutritional-calculator.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+import { EntreeComponent } from './entree/entree.component';
+import { IngredientListComponent } from './ingredient-list/ingredient-list.component';
+import { MenuComponent } from './menu/menu.component';
 import { NutritionalCalculatorComponent } from './nutritional-calculator.component';
+import { NutritionalFactsComponent } from './nutritional-facts/nutritional-facts.component';
+
 @NgModule({
   declarations: [
-    NutritionalCalculatorComponent
+    EntreeComponent,
+    IngredientListComponent,
+    MenuComponent,
+    NutritionalCalculatorComponent,
+    NutritionalFactsComponent
   ],
   imports: [
     SharedModule
