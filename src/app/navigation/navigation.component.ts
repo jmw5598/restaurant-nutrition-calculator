d72b3557d51634856f242be8ba963bb6d31dd712
Jason White <jmw5598@gmail.com>
2018-02-19 19:52:23 -0500

    created navigation bar

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'qdoba-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+
+  constructor() {}
+
+}
---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { NavigationComponent } from './navigation.component';
+
+@NgModule({
+  declarations: [
+    NavigationComponent
+  ],
+  imports: [],
+  exports: [
+    NavigationComponent
+  ],
+  providers: []
+})
+export class NavigationModule{}
